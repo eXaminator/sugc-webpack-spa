25583ee5d4d21513826a3dd68c028bf3226bdb4b
Steven Weingärtner <sweingaertner@brainbits.net>
2017-09-13 10:07:17 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+    },
+    module: {
+        rules: [
+            { test: /\.html$/, use: 'html-loader' },
+            { test: /\.(jpeg|png)$/, use: 'file-loader' },
+        ],
+    },
+};
